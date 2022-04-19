d7b116e5677da9a7d8b8c7ea3efc273724a20b14
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2022-04-19 14:45:20 +0430

    working on the main page

--- resources/views/livewire/welcome-page.blade.php
+++ resources/views/livewire/welcome-page.blade.php
@@ -1,5 +1,179 @@
-<div>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+<div class="bg-white">
+    <div class="bg-cover h-screen"
+        style="background-image: url('https://images.unsplash.com/photo-1574680096145-d05b474e2155?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80')">
+        <div
+            class="max-w-2xl mx-auto py-24 px-4 grid items-center grid-cols-1 gap-y-16 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
+            <div>
+                <h1 class="text-3xl font-extrabold tracking-tight text-white sm:text-5xl">NO PAIN NO GAIN</h1>
+                <p class="mt-14 text-gray-200">Having a perfect body requires a lot of training. Nice-looking body and
+                    powerful organism are interconnected – and we can help you with both.</p>
+            </div>
+        </div>
+    </div>
+    <div class="py-24">
+        <p class="text-5xl font-extrabold text-center">welcome</p>
+        <p class="mt-14 text-gray-900 text-center">Welcome to the website Intense Gym! We hope that you will appreciate
+            our services
+            and opportunities we offer<br /> our loyal and potential customers. Here are some of them:</p>
+    </div>
+    <div class="flex -space-x-1 overflow-hidden justify-evenly">
+        <div>
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px" y="0px" width="75.695px" height="75.695px" viewBox="0 0 35.695 35.695"
+                style="enable-background:new 0 0 35.695 35.695; margin-left: 5rem;" xml:space="preserve">
+                <g>
+                    <path d="M11.558,10.767c0-3.473,2.815-6.29,6.289-6.29c3.476,0,6.289,2.817,6.289,6.29c0,3.475-2.813,6.29-6.289,6.29
+  C14.373,17.057,11.558,14.243,11.558,10.767z M35.667,22.607l-0.879-5.27c-0.158-0.954-0.961-1.754-1.896-1.984
+  c-0.836,0.74-1.932,1.191-3.136,1.191c-1.203,0-2.3-0.452-3.135-1.191c-0.938,0.229-1.739,1.03-1.897,1.984l-0.021,0.124
+  c-0.522-0.503-1.17-0.881-1.868-1.052c-1.33,1.176-3.072,1.896-4.987,1.896s-3.657-0.72-4.987-1.896
+  c-0.698,0.171-1.346,0.549-1.868,1.052l-0.021-0.124c-0.158-0.954-0.962-1.754-1.897-1.984c-0.835,0.74-1.932,1.191-3.135,1.191
+  c-1.204,0-2.3-0.452-3.136-1.191c-0.936,0.229-1.738,1.03-1.896,1.984l-0.879,5.27c-0.189,1.131,0.596,2.057,1.741,2.057h7.222
+  l-0.548,3.283c-0.3,1.799,0.948,3.271,2.771,3.271H24.48c1.823,0,3.071-1.475,2.771-3.271l-0.548-3.283h7.222
+  C35.071,24.662,35.855,23.738,35.667,22.607z M29.755,15.762c2.184,0,3.954-1.77,3.954-3.954c0-2.183-1.771-3.954-3.954-3.954
+  s-3.953,1.771-3.953,3.954C25.802,13.992,27.574,15.762,29.755,15.762z M5.938,15.762c2.183,0,3.953-1.77,3.953-3.954
+  c0-2.183-1.771-3.954-3.953-3.954c-2.184,0-3.954,1.771-3.954,3.954C1.984,13.992,3.755,15.762,5.938,15.762z" />
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+            </svg>
+            <p class="text-3xl font-extrabold text-red-600">Qualified Coaches</p>
+            <p class="text-medium font-medium text-gray-600 text-center">Our coaches have years of experience<br /> in
+                various
+                types of fitness and sports.</p>
+        </div>
+        <div>
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="62.695px" height="62.695px"
+                viewBox="0 0 512 512" style="enable-background:new 0 0 512 512; margin-left: 7rem;"
+                xml:space="preserve">
+                <g>
+                    <g>
+                        <g>
+                            <path d="M160,224H53.333C23.936,224,0,247.936,0,277.333V448c0,29.397,23.936,53.333,53.333,53.333h64
+    c29.397,0,53.333-23.936,53.333-53.333V234.667C170.667,228.779,165.888,224,160,224z" />
+                            <path d="M512,304c0-12.821-5.099-24.768-13.867-33.6c9.963-10.901,15.04-25.536,13.632-40.725
+    c-2.496-27.115-26.944-48.341-55.637-48.341H324.373c6.507-19.819,16.96-56.149,16.96-85.333c0-46.272-39.317-85.333-64-85.333
+    c-22.165,0-37.995,12.459-38.677,12.992c-2.539,2.027-3.989,5.099-3.989,8.341v72.32l-47.957,103.893
+    c-1.472,3.221-1.28,6.955,0.555,9.984c3.136,5.184,4.736,10.731,4.736,16.448v213.333c0,5.141-0.597,10.368-1.835,16.021
+    c-1.259,5.739,2.347,11.413,8.064,12.693c9.365,2.112,18.539,3.285,25.771,3.285h195.819c23.232,0,43.563-15.659,48.32-37.269
+    c2.453-11.115,1.003-22.336-3.84-32.043c15.765-7.936,26.368-24.192,26.368-42.688c0-7.531-1.728-14.784-5.013-21.333
+    C501.397,338.731,512,322.496,512,304z" />
+                        </g>
+                    </g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+            </svg>
+            <p class="mt-3 text-3xl font-extrabold text-red-600">Individual Approach</p>
+            <p class="text-medium font-medium text-gray-600 text-center">Every client of Intense has a
+                personalized<br />
+                program of training and nutrition.</p>
+        </div>
+        <div>
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="78.695px" height="78.695px"
+                viewBox="0 0 472.615 472.615" style="enable-background:new 0 0 472.615 472.615; margin-left: 10rem;"
+                xml:space="preserve">
+                <g>
+                    <g>
+                        <path d="M454.157,207.697h-20.765v-55.651h-57.312v-50.204h-57.311v105.855H153.846V101.842H96.535v50.204H39.223v55.651H18.457
+   C8.264,207.697,0,215.96,0,226.154v20.305c0,10.193,8.264,18.457,18.457,18.457h20.766v55.56l57.312,0.065v50.233h57.221V264.916
+   h165.014v105.858h57.311v-50.207h57.312v-55.651h20.765c10.195,0,18.459-8.264,18.459-18.457v-20.305
+   C472.615,215.96,464.351,207.697,454.157,207.697z" />
+                    </g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+            </svg>
+            <p class="text-3xl font-bold text-red-600">Modern Fitness Equipment</p>
+            <p class="text-medium font-medium text-gray-600 text-center">We cooperate with leading fitness
+                equipment<br />
+                suppliers to
+                give you the superior results.</p>
+        </div>
+    </div>
 </div>
